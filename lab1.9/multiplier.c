2d2b44a654f1bde2bb29c15c36cbf22973f4ca57
Karol Gutierrez <oscreader@OSC>
2018-09-21 22:50:41 -0600

    New changes

--- lab1.9/multiplier.c
+++ lab1.9/multiplier.c
@@ -1,7 +1,8 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include <pthread.h>
-#include<unistd.h>
+
+#define N 2000
 
 
 //global variables
@@ -14,28 +15,82 @@ long *result;
 long *readMatrix(char* filename){
 	int i;
 	long *mat;
-	mat=(long*)malloc(4000000*sizeof(long));
+	mat=(long*)malloc(N*N*sizeof(long));
 	FILE *f;
 	f=fopen(filename, "r");
-	for(i=0; i<4000000; i++){
-		fscanf(f, "%ld", &mat[i]);	
+	for(i=0; i<9; i++){
+		fscanf(f, "%ld", &mat[i]);
 	}
 	fclose(f);
 	return mat;
 }
-long *getColumn(int col, long*matrix);
-long *getRow(int row, long * matrix);
-int getLock();
-int releaseLock(int lock);
-long dotProduct(long *vec1, long *vec2){//assuming that the size is 2000
+void printMatrix(long *matrix, int len){
+  int i, j;
+  for(i=0; i<len; i++){
+    for(j=0; j<len; j++){
+      printf("%ld ", matrix[len*i+j]);
+    }
+    printf("\n");
+
+  }
+}
+long *getColumn(int col, long*matrix){
+	int i;
+	long *ans=(long*)malloc(N*sizeof(long));
+	for(i=0; i<N; i++){
+		ans[i]=matrix[i*N+col];
+	}
+	return ans;
+}
+long *getRow(int row, long * matrix){
+	int i;
+	long *ans=(long*)malloc(N*sizeof(long));
+	for(i=0; i<N; i++){
+		ans[i]=matrix[row*N+i];
+	}
+	return ans;
+}
+long dotProduct(long *vec1, long *vec2){
 	long i, ans=0;
-	for(i=0; i<2000; i++){
+	for(i=0; i<N; i++){
 		ans+=vec1[i]*vec2[i];
 	}
 	return ans;
 }
-long *multiply(long *matA, long *matB);
-int saveResultMatrix(long *result);
+int saveResultMatrix(){
+  int i;
+	FILE *f;
+	f=fopen("result.dat", "w");
+	for(i=0; i<N*N; i++){
+		fprintf(f, "%ld\n", result[i]);
+	}
+	fclose(f);
+  return 0;
+}
+int getLock(){
+	int i;
+	for(i=0; i<NUM_BUFFERS; i++)
+		if(0==pthread_mutex_trylock(&mutexes[i])) return i;
+	return -1;
+}
+int releaseLock(int lock){
+	if(0==(pthread_mutex_unlock(&mutexes[lock]))) return 0;
+		return -1;	
+}
+
+long *multiply(long *matA, long *matB){
+	int i, j;
+	long *rr=malloc(N*sizeof(long));
+	long *cc=malloc(N*sizeof(long));
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			rr=getRow(i, matA);
+			cc=getColumn(j, matB);
+			result[N*i+j]=dotProduct(rr,cc);
+		}
+	}
+	return result;
+}
 
 
 int main(int argc, char *argv[]){
@@ -46,14 +101,26 @@ int main(int argc, char *argv[]){
 		return 1; //error
 	}
 	int NUM_BUFFERS=atoi(a);
-	printf("The number of buffers is %d\n", NUM_BUFFERS);
 
 	long* matA=readMatrix("matA.dat");
 	long* matB=readMatrix("matB.dat");
-	
-	printf("%ld",matA[1597829]+matB[1597829]); //the scan works
 
+	/* //se alenta mucho al usar mutexes pues se generan copias innecesarias
+	mutexes=malloc(NUM_BUFFERS*sizeof(pthread_mutex_t));
+	buffers=malloc(NUM_BUFFERS*sizeof(long*));
+	for(i=0;i<NUM_BUFFERS;i++){
+		buffers[i]=malloc(N*sizeof(long));
+	}
+  result=malloc(N*N*sizeof(long));
+	*/
 
-free(matA);
-free(matB);
+	multiply(matA, matB);
+	saveResultMatrix();
+
+	free(matA);
+	free(matB);
+	free(result);
+	//free(buffers);
+	//pthread_mutex_destroy(mutexes);
+	return 0;
 }
