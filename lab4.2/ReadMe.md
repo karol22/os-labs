4b42dbf4bafa4d5f5cff815ebe57cb59df0eb218
Karol Gutierrez <oscreader@OSC>
2018-10-05 21:38:15 -0600

    add lab

--- lab4.2/ReadMe.md
+++ lab4.2/ReadMe.md
@@ -3,7 +3,7 @@ Diego Ivan Contreras Villegas A00817130
 Daniela Abigail Parrale Mejía A01228629
 Karol Gutiérrez A01024536
 
-Compile with the following command:
-gcc dinning_philosophers.c -o dinner -lpthread
+Compile with the following commands:
+gcc -o dinning_philosophers dinning_philosophers.c -lpthread
 
 Stop the program with Ctrl+Z
--- lab4.2/dinning_philosophers.c
+++ lab4.2/dinning_philosophers.c
@@ -4,8 +4,8 @@ Diego Ivan Contreras Villegas A00817130
 Daniela Abigail Parrale Mejía A01228629
 Karol Gutiérrez A01024536
 
-Compile with the following command:
-gcc dinning_philosophers.c -o dinner -lpthread
+Compile with the following commands:
+gcc -o dinning_philosophers dinning_philosophers.c -lpthread
 
 Stop the program with Ctrl+Z
 
@@ -26,55 +26,55 @@ int phil[N];
 sem_t mutex; 
 sem_t S[N]; 
   
-void test(int phnum){ 
-    if (state[phnum] == HUNGRY && state[(phnum+4)%N] != EATING && state[(phnum+1)%N] != EATING) { 
+void test(int phils){ 
+    if (state[phils] == HUNGRY && state[(phils+4)%N] != EATING && state[(phils+1)%N] != EATING) { 
         // state that eating 
-        state[phnum] = EATING; 
+        state[phils] = EATING; 
   
         sleep(2); 
   
-        printf("Philosopher %d takes fork %d and %d\n", phnum + 1, (phnum+4)%N+1, phnum + 1); 
-        printf("Philosopher %d is Eating\n", phnum + 1); 
+        printf("Philosopher %d takes forks number #%d and #%d\n", phils + 1, (phils+4)%N+1, phils + 1); 
+        printf("Philosopher %d is eating\n", phils + 1); 
         
-        sem_post(&S[phnum]); 
+        sem_post(&S[phils]); 
     } 
 } 
   
 // take up chopsticks 
-void take_fork(int phnum) { 
+void take_fork(int phils) { 
   
     sem_wait(&mutex); 
   
     // Define as hungry
-    state[phnum] = HUNGRY; 
+    state[phils] = HUNGRY; 
   
-    printf("Philosopher %d is Hungry\n", phnum + 1); 
+    printf("Philosopher %d is Hungry\n", phils + 1); 
   
     //Eat if neighbours are not eating 
-    test(phnum); 
+    test(phils); 
   
     sem_post(&mutex); 
   
     //wait to be signalled if unable to eat 
-    sem_wait(&S[phnum]); 
+    sem_wait(&S[phils]); 
   
     sleep(1); 
 } 
   
 // put down chopsticks 
-void put_fork(int phnum) { 
+void put_fork(int phils) { 
   
     sem_wait(&mutex); 
   
     // state that thinking 
-    state[phnum] = THINKING; 
+    state[phils] = THINKING; 
   
     printf("Philosopher %d putting fork %d and %d down\n", 
-           phnum + 1, (phnum+4)%N+1, phnum + 1); 
-    printf("Philosopher %d is thinking\n", phnum + 1); 
+           phils + 1, (phils+4)%N+1, phils + 1); 
+    printf("Philosopher %d is thinking\n", phils + 1); 
   
-    test((phnum+4)%N); 
-    test((phnum+1)%N); 
+    test((phils+4)%N); 
+    test((phils+1)%N); 
   
     sem_post(&mutex); 
 } 
